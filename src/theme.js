5bcb65d81cae076037aaac270f34d03679ad4280
akinghill <austinkinghill@gmail.com>
2020-12-30 15:01:15 -0700

    Theme Update

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,12 @@
+import { cyan } from '@material-ui/core/colors';
 import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
     type: "dark",
+    secondary: {
+      main: cyan[500],
+    },
   },
 });
 
